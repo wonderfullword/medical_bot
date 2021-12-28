708011f52a162598ee45c643863b18644d8d7ef5
Shevelinskaya_Anna <annashevelinskaya825@gmail.com>
2021-12-28 23:12:54 +0300

    echo bot

---
+++ bot.py
@@ -0,0 +1,21 @@
+import config
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+# log level
+logging.basicConfig(level=logging.INFO)
+# bot init
+bot=Bot(token=config.TOKEN)
+dp=Dispatcher(bot)
+
+# echo
+@dp.message_handler()
+async def echo(message:types.Message):
+    await message.answer(message.text)
+
+# run long-polling
+if __name__=="__main__":
+    executor.start_polling(dp,skip_updates=True)
+
+inline
